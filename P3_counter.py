1fc1c6fd4fcd003b9fd24dffd07aca85240367e1
Arnav Rathore <arnav005singhrathore@gmail.com>
2026-07-06 02:17:44 +0530

    Add short note

--- P3_counter.py
+++ P3_counter.py
@@ -1,3 +1,7 @@
-""" LETTERS COUNTER :- """
+""" 
+SIMPLE CHARACTER COUNTER
+Feature - Helps to count characters in the paragraph or any sentence.
+"""
+
 k = input("Write whatever you want : ")
 print("your sentence has",len(k),"characters.")
